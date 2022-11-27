0296a0f5348b3df2cd5ae27e96af1bbdeb5dd314
Ekaterina <zebraline@mail.ru>
2022-11-27 20:43:23 +0500

    Добавили папку Task_2

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,3 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a >= b) 
+{
+    Console.Write($"max = {a}");
+}
+else
+{
+    Console.Write($"max = {b}");
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.Write("Введите целое число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число C: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int d = a;
+if(b >= d) 
+{
+    d = b;
+}
+if(c>=d)
+{
+    d = c;
+    Console.WriteLine(d);
+}
+else
+{
+    Console.WriteLine(d);
+}
