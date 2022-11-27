eea4585dee19b074531a58005818716797e546a9
Ekaterina <zebraline@mail.ru>
2022-11-27 21:38:46 +0500

    Переименованы папки по первым двум задачам

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -3,19 +3,11 @@ Console.Write("Введите целое число А: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите целое число B: ");
 int b = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите целое число C: ");
-int c = Convert.ToInt32(Console.ReadLine());
-int d = a;
-if(b >= d) 
+if(a >= b) 
 {
-    d = b;
-}
-if(c>=d)
-{
-    d = c;
-    Console.WriteLine(d);
+    Console.Write($"max = {a}");
 }
 else
 {
-    Console.WriteLine(d);
+    Console.Write($"max = {b}");
 }
--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -3,11 +3,19 @@ Console.Write("Введите целое число А: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите целое число B: ");
 int b = Convert.ToInt32(Console.ReadLine());
-if(a >= b) 
+Console.Write("Введите целое число C: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int d = a;
+if(b >= d) 
 {
-    Console.Write($"max = {a}");
+    d = b;
+}
+if(c>=d)
+{
+    d = c;
+    Console.WriteLine(d);
 }
 else
 {
-    Console.Write($"max = {b}");
+    Console.WriteLine(d);
 }
