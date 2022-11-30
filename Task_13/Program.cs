d6eb23973a157f6edbc6379280d8282ea41c6204
Ekaterina <zebraline@mail.ru>
2022-12-01 00:52:48 +0500

    HomeWork done

---
+++ Task_13/Program.cs
@@ -0,0 +1,16 @@
+﻿// Task 13
+Console.Clear();
+Console.Write("Введите целое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = 0;
+if (Math.Abs(n) < 100)
+    Console.WriteLine("Третья цифра числа отсутствует");
+else
+{
+    while (Math.Abs(n) >= 100)
+    {
+        m = n % 10;
+        n = n / 10;
+    }
+    Console.WriteLine($"Третья цифра числа: {Math.Abs(m)}");
+}
