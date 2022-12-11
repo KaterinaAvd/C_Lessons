72e353e49f88536c6b368f2598f53d4bfbec84a9
Ekaterina <zebraline@mail.ru>
2022-12-12 00:21:11 +0500

    Sixth HomeWork Done

---
+++ Task_41/Program.cs
@@ -0,0 +1,16 @@
+﻿// Task_41
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[M];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите {i+1} число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"Вы ввели {M} чисел: {string.Join(", ", array)}");
+int n = 0;
+for (int i = 0; i < array.Length; i++)
+    if (array[i] > 0)
+        n = n + 1;
+Console.WriteLine($"Среди них {n} чисел больше нуля");
---
+++ Task_43/Program.cs
@@ -0,0 +1,15 @@
+﻿// Task_43
+Console.Clear();
+Console.WriteLine("Введите коэффициенты прямой y = k1*x + b1");
+Console.Write("k1 = ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("b1 = ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите коэффициенты прямой y = k2*x + b2");
+Console.Write("k2 = ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("b2 = ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+double x = Convert.ToDouble((b2 - b1) / (k1 - k2));
+double y = Convert.ToDouble((k1*((b2 - b1) / (k1 - k2)))+ b1);
+Console.WriteLine($"Прямые пересекаются в точке ({x}; {y})");
