861f58bf9763939b788e26bae72632ca32c0ab1c
Ekaterina <zebraline@mail.ru>
2022-12-12 23:31:18 +0500

    Task_36 changed

--- Task_36/Program.cs
+++ Task_36/Program.cs
@@ -7,9 +7,6 @@ for (int i = 0; i < array.Length; i++)
 array[i] = new Random().Next(1, 1000); // [1, 999]
 Console.WriteLine($"Случайный массив из {N} элементов: [{string.Join(", ", array)}]");
 int amount = 0;
-for (int k = 0; k < array.Length;)
-{
+for (int k = 1; k < array.Length; k = k + 2)
         amount = amount + array[k];
-        k = k + 2;
-}
 Console.WriteLine($"Сумма элементов массива, стоящих на нечетных местах: {amount}");
