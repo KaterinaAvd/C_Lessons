024816158d4552a73e0d687e43a0f8e9d1494daa
Ekaterina <zebraline@mail.ru>
2023-01-09 20:21:28 +0500

    9th HomeWork

---
+++ Task_66/Program.cs
@@ -0,0 +1,15 @@
+﻿//Task_66
+
+int rec(int M, int N)
+{
+    if (M == N)
+        return N;
+    return rec(M, N - 1) + N;
+}
+
+Console.Clear();
+Console.WriteLine("Введите первое натуральное число:");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе натуральное число (оно должно быть больше первого)");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма натуральных чисел между ними (включительно): {rec(M, N)}");
---
+++ Task_68/Program.cs
@@ -0,0 +1,18 @@
+﻿//Task_68
+
+int rec(int m, int n)
+{
+        if (m == 0) 
+            return n + 1;
+        else if (n == 0 && m > 0) 
+            return rec(m - 1, 1);
+        else
+            return rec(m - 1, rec(m, n - 1));
+}
+
+Console.Clear();
+Console.WriteLine("Введите первое натуральное число:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе натуральное число:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"A (m, n) = {rec(m, n)}");
